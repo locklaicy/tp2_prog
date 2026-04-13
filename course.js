96b16e785390a11bc51dc3694f7d15cea74ffbc8
Théana L. <theana.leurot@gmail.com>
2026-04-13 11:22:54 -0400

    finish css html

--- course.js
+++ course.js
@@ -1,2 +1,31 @@
-let voiture1 = document.getElementById(voiture1);
-let voiture2 = document.getElementById(voiture2);
+let voiture1 = document.getElementsByClassName(voiture1);
+let voiture2 = document.getElementsByClassName(voiture2);
+
+let translationX1;
+let translationX2;
+
+let containerWidth = 
+
+let voiture1Width =
+let voiture2Width =
+
+
+function compteARebour() {
+
+}
+
+function chronometre() {
+
+}
+
+function aleatoireTranslationX() {
+
+}
+
+function deplacerVoiture() {
+
+}
+
+function demarrerCourse() {
+
+}
