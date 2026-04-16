6f73ccd0581aba433233b16ed09077bcd410f54e
Flasur <colindube31@gmail.com>
2026-04-16 11:10:28 -0400

    peut etre

--- course.js
+++ course.js
@@ -1,51 +1,70 @@
-let voiture1 = document.querySelector(".voiture1");
-let voiture2 = document.querySelector(".voiture2");
-let container = document.querySelector(".course-container");
+let voiture1 = document.getElementsByClassName(voiture1);
+let voiture2 = document.getElementsByClassName(voiture2);
 
 let translationX1;
 let translationX2;
 
-let containerWidth = container.offsetWidth;
+let containerWidth = course-container.offsetWidth;
 
 let voiture1Width = voiture1.offsetWidth;
 let voiture2Width = voiture2.offsetWidth;
 
 
 function compteARebour() {
-
+  
 }
 
 function chronometre() {
 
 }
 
-function aleatoireTranslationX1(voiture1Width) {
+function aleatoireTranslationX() {
     let maxTranslationX1 = containerWidth - voiture1Width;
-    return Math.floor(Math.random() * (maxTranslationX1 + 1));
-}
-
-function aleatoireTranslationX2(voiture2Width) {
     let maxTranslationX2 = containerWidth - voiture2Width;
-    return Math.floor(Math.random() * (maxTranslationX2 + 1));
-}
 
+    return Math.floor(Math.random() * (maxTranslationX1 + 1)) + Math.floor(Math.random() * (maxTranslationX2 + 1));
+
+}
 
 function deplacerVoiture() {
-    translationX1 = translationX1 + aleatoireTranslationX1();
-    translationX2 = translationX2 + aleatoireTranslationX2();
-
-    if (translationX1 >= maxTranslationX1) {
-        translationX1 = maxTranslationX1;
-    }
-
-    if (translationX2 > maxTranslationX2) {
-        translationX2 = maxTranslationX2;
-    }
-
-    voiture1.style.left = translationX1 + "px";
-    voiture2.style.left = translationX2 + "px";
+    
 }
 
 function demarrerCourse() {
 
 }
+
+
+
+
+
+
+/*
+<script>
+  let countdown; // To store the interval ID
+
+  document.getElementById("startBtn").addEventListener("click", function () {
+    let timeLeft = 10; // seconds
+    const timerDisplay = document.getElementById("timer");
+
+    // Clear any existing timer
+    clearInterval(countdown);
+
+    // Immediately show starting time
+    timerDisplay.textContent = timeLeft;
+
+    // Start countdown
+    countdown = setInterval(() => {
+      timeLeft--;
+      timerDisplay.textContent = timeLeft;
+
+      if (timeLeft <= 0) {
+        clearInterval(countdown);
+        alert("Time's up!");
+      }
+    }, 1000); // 1000 ms = 1 second
+  });
+</script>
+
+*/
+
